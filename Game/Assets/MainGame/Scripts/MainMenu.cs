5a496b42b48bd0c2df685c6332e41d1fae9c915d
gxdshonour <b00139397@mytudublin.ie>
2022-12-19 22:09:18 +0000

    project

---
+++ Game/Assets/MainGame/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+
+{
+
+    public void OpenChannel1()
+    {
+        Application.OpenURL("https://christopherv21.github.io/challenge5/");
+    }
+
+    public void OpenChannel2()
+    {
+        Application.OpenURL("https://christopherv21.github.io/challenge5/");
+    }
+
+   
+}
+
+
+
